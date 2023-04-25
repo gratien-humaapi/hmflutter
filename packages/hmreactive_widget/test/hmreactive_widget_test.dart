aead98405398e1f3eb74ce74e074556501f75433
gratien-humaapi <86977154+gratien-humaapi@users.noreply.github.com>
2023-04-25 13:53:58 +0100

    first-commit

---
+++ packages/hmreactive_widget/test/hmreactive_widget_test.dart
@@ -0,0 +1,16 @@
+import 'package:hmreactive_widget/hmreactive_widget.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
